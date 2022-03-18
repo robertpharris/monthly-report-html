5bc15e33be6d89ad47b5b51474d89c9c8420f968
robert.pharris <robert.pharris@longroadenergy.com>
2022-03-18 14:32:15 -0600

    Initial commit

---
+++ template_builder.py
@@ -0,0 +1,14 @@
+table_list = {'Table':{1.1:'KPI Summary',2.1:'Key Initiatives', 3.1: 'Safety Summary',
+                       4.1: 'MTD Financial Performance',
+              4.2:'YTD Financial Performance', 4.3:'REC Summary', 5.1:'Site Summary', 5.2: 'Lost Energy',
+                       6.1: 'Turbine Performance', 6.2: 'Offline and Curtailed (current month over 7 days)',
+                       6.3:'top Turbine Fault and Event States', 7.1: 'Grid Operator Curtailment Summary',
+                       7.2: 'Balance of Plant Outages', 8.1: 'Maintenance Summary', 9.1: 'Compliance Log',
+                       10.1:'Definitions'},
+              'Figure': {5.1:'Monthly Energy Production', 5.2:'Daily Wind Speed and Project Availability',
+                         },
+              'Section': {1: 'Executive Summary', 2: 'Operations Management', 3: 'Safety',
+                          4: 'Financial Performance', 5: 'Site Performance', 6: 'Turbine Performance',
+                          7: 'Balance of Plant', 8: 'Planned Maintenance', 9:'Compliance', 10: 'Definitions'}
+
+}
